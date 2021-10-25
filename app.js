6b34d55d117a3764ad64dd5fa365ecc4dc9a8869
Enrique Elizondo <quiqueev@gmail.com>
2021-10-25 17:27:29 -0500

    Modified app.js line 12

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 3000;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+} else {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
--- src/App.js
+++ src/App.js
@@ -39,7 +39,9 @@ const App = () => {
         setLoading(true);
 
         response = await fetch(urlExtension);
+
         console.log(response); // Check
+
         data = await response.json();
 
         const ht = data.Results[0];
